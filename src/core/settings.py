b586cc39bd3ead61d4d604f4ee81fb19c5d0a310
BRANYA43 <tbd2504@gmai.com>
2024-10-27 12:11:25 +0200

    Add skip_integration_tests to Settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).parent.parent
 class Settings(BaseSettings):
     openai_api_token: str
     github_api_token: str
+    skip_integration_tests: bool = False
 
     model_config = SettingsConfigDict(env_file=BASE_DIR / '../.env')
 
