d4beb561953bc92b0d1e0a61cd8621332509d3f7
BRANYA43 <tbd2504@gmai.com>
2024-10-27 11:57:49 +0200

    Create GitHubRepo

---
+++ src/integrations/github.py
@@ -0,0 +1,74 @@
+import re
+from collections import deque
+from types import MappingProxyType
+from typing import cast
+
+import httpx
+from pydantic import BaseModel, Base64Str
+
+from core.settings import settings
+
+GITHUB_URL_PATTERN = re.compile(r'^https://github\.com/([^/]+)/([^/]+)$')
+REPO_URL_PATTERN = r'https://api.github.com/repos/{owner}/{repo}/contents'
+HEADERS = MappingProxyType(
+    {'Authorization': f'Bearer {settings.github_api_token}', 'Accept': 'application/vnd.github.v3+json'}
+)
+
+
+class GitHubFile(BaseModel):
+    path: str
+    content: Base64Str
+
+    def __str__(self):
+        return f'{self.path}: {self.content}'
+
+
+class GitHubRepo:
+    def __init__(self, url: str):
+        self._validate_url(url)
+        self._url = url
+        match_ = cast(re.Match[str], GITHUB_URL_PATTERN.match(self._url))
+        self._owner, self._repo = match_.groups()
+        self._content_url = REPO_URL_PATTERN.format(owner=self._owner, repo=self._repo)
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def owner(self) -> str:
+        return self._owner
+
+    @property
+    def repo(self) -> str:
+        return self._repo
+
+    async def fetch_content(self, client: httpx.AsyncClient) -> list[str]:
+        """Returns all content of repo."""
+        content: list[str] = []
+        response = await self._http_get(client, self._content_url)
+        response_deque = deque(response)
+        while response_deque:
+            pop = response_deque.popleft()
+            if pop['type'] == 'file':
+                file_response = await self._http_get(client, pop['url'])
+                file_response = cast(dict, file_response)
+                file = GitHubFile(path=file_response['path'], content=file_response['content'])
+                content.append(str(file))
+            elif pop['type'] == 'dir':
+                dir_response = await self._http_get(client, pop['url'])
+                response_deque.extendleft(dir_response)
+
+        return content
+
+    @staticmethod
+    async def _http_get(client: httpx.AsyncClient, url, headers=HEADERS, timeout=10, **kwargs) -> list[dict] | dict:
+        """Wrapper under httpx.AsyncClient.get"""
+        response = await client.get(url=url, headers=headers, timeout=timeout, **kwargs)
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def _validate_url(url):
+        if re.match(GITHUB_URL_PATTERN, url) is None:
+            raise ValueError('Expected a url of GitHub repository of user. Example: https://github.com/{owner}/{repo}')
---
+++ src/tests/test_integrations/test_github.py
@@ -0,0 +1,61 @@
+from unittest.mock import patch, AsyncMock
+
+import httpx
+import pytest
+from httpx import Request
+
+from integrations.github import GitHubRepo
+
+
+class TestGitHubRepo:
+    OWNER = 'bob'
+    REPO = 'cli_app'
+    REPO_URL = f'https://github.com/{OWNER}/{REPO}'
+    CONTENT_REPO_URL = f'https://api.github.com/repos/{OWNER}/{REPO}/contents'
+    INVALID_REPO_URL = f'https://invalid-url.com/{OWNER}/{REPO}'
+
+    @pytest.mark.asyncio
+    async def test_creating_repo_with_valid_url(self):
+        repo = GitHubRepo(self.REPO_URL)
+        assert repo.url == self.REPO_URL
+        assert repo.owner == self.OWNER
+        assert repo.repo == self.REPO
+
+    @pytest.mark.asyncio
+    async def test_creating_repo_raises_error_for_invalid_url(self):
+        with pytest.raises(
+            ValueError,
+            match='Expected a url of GitHub repository of user. Example: https://github.com/{owner}/{repo}',
+        ):
+            GitHubRepo(self.INVALID_REPO_URL)
+
+    @pytest.mark.asyncio
+    @patch.object(httpx.AsyncClient, 'get', new_callable=AsyncMock)
+    async def test_fetching_content_raises_error_if_status_code_404(self, mock_get):
+        mock_get.return_value = httpx.Response(404, request=Request('get', self.CONTENT_REPO_URL))
+
+        repo = GitHubRepo(self.REPO_URL)
+
+        async with httpx.AsyncClient() as client:
+            with pytest.raises(httpx.HTTPStatusError):
+                await repo.fetch_content(client)
+
+    @pytest.mark.asyncio
+    @patch.object(GitHubRepo, '_http_get', new_callable=AsyncMock)
+    async def test_fetching_content_returns_content(self, mock_get):
+        mock_response_values = {
+            self.CONTENT_REPO_URL: [
+                {'type': 'file', 'path': 'file1.txt', 'url': 'file1_url'},
+                {'type': 'dir', 'path': 'texts', 'url': 'dir_url'},
+            ],
+            'file1_url': {'path': 'file1.txt', 'content': bytes('SGVsbG8gV29ybGQ=', 'utf-8')},
+            'dir_url': [{'type': 'file', 'path': 'file2.txt', 'url': 'file2_url'}],
+            'file2_url': {'path': 'file2.txt', 'content': bytes('SGVsbG8gUHl0aG9u', 'utf-8')},
+        }
+        mock_get.side_effect = lambda client_, key, *args, **kwargs: mock_response_values[key]
+
+        repo = GitHubRepo(self.REPO_URL)
+        async with httpx.AsyncClient() as client:
+            content = await repo.fetch_content(client)
+
+        assert content == ['file1.txt: Hello World', 'file2.txt: Hello Python']
